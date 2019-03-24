fba7be7ce8aac44ff7746b05919974e693ba36f1
linw <770601773@qq.com>
2019-03-24 18:09:50 +0800

    Initial commit

---
+++ app/src/main/java/com/lvcd/hotfixdemoapp/MyApplication.java
@@ -0,0 +1,11 @@
+package com.lvcd.hotfixdemoapp;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
